4bdba5aad6d3877c657e0bf7f2f4a908f64145af
Rodrigo Bossini <professorbossini@gmail.com>
2022-05-10 19:21:22 -0300

    exercicios: exercicio 01

---
+++ script.R
@@ -0,0 +1,5 @@
+#1. Abra o arquivo cancer.txt como um data frame e faça um sumário estatístico dos dados.
+arq <- read.table(file = "cancer.txt", header = T);arq
+summary(arq)
+##########################################################################
+
