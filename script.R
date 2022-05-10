d6b7d543ca4fdfbbd230129a888456b7a3fedf4a
Rodrigo Bossini <professorbossini@gmail.com>
2022-05-10 19:31:41 -0300

    exercicios: exercicio 02

--- script.R
+++ script.R
@@ -2,4 +2,33 @@
 arq <- read.table(file = "cancer.txt", header = T);arq
 summary(arq)
 ##########################################################################
+#2. Faça o histograma das variáveis LDH, ALB e N. Use a descrição da coluna como título de cada um. Use cores diferentes.
 
+#ajustar os parâmetros de gráficos
+par(mfrow=c(1, 3))
+
+hist(
+  arq[, 'LDH'], 
+  main = "Enzima, lactate dehydrogenase", 
+  xlab = "LDH",
+  ylab = "frequência absoluta",
+  col = 'red'
+)
+
+hist(
+  arq[, 'ALB'],
+  main = "Albumina",
+  xlab = "ALB",
+  ylab = "frequência absoluta",
+  col = "blue"
+)
+
+hist(
+  arq[, 'N'],
+  main = "Nitrogênio na uréia",
+  xlab = "N",
+  ylab = "frequência absoluta",
+  col = "green"
+)
+par(mfrow=c(1, 1))
+############################################################
